bee1c4e15ade7d590f9f3115fec4529e63a35591
Chad <wsrfssx@yeah.net>
2024-02-29 10:57:48 +0800

    fix: 呼吸灯修改时间配置

--- A001.2.led_breath/rtl/led_breath.v
+++ A001.2.led_breath/rtl/led_breath.v
@@ -12,9 +12,10 @@ localparam DELAY_1us = 'd3;
 localparam K = 'd10;
 */
 
-localparam DELAY_1us = 'd33;
+localparam DELAY_1us = 'd33_333_333;
 localparam K = 'd1000;
 
+reg flag; // 转换标志
 reg [ 6:0] cnt;
 reg [ 9:0] cnt_1us; // 1us 好和1ms计数作对比，产生pwm
 reg [ 9:0] cnt_1ms;
@@ -22,9 +23,9 @@ reg [ 9:0] cnt_1ms;
 // 1us 计时
 always @(posedge s_clk or negedge s_rst) begin
     if(s_rst == 1'b0) 
-        cnt = 0;
+        cnt = 1'b0;
     else if(cnt == DELAY_1us)
-        cnt = 0;
+        cnt = 1'b0;
     else
         cnt = cnt + 1'b1;
 end
@@ -32,33 +33,41 @@ end
 // 1us 计数
 always @(posedge s_clk or negedge s_rst) begin
     if(s_rst == 1'b0)
-        cnt_1us = 0;
-    else if(cnt==0 && (cnt_1us == K-1))
-        cnt_1us = 0;
-    else if(cnt == 0)
+        cnt_1us = 1'b0;
+    else if(cnt==1'b0 && (cnt_1us == K-1))
+        cnt_1us = 1'b0;
+    else if(cnt == 1'b0)
         cnt_1us = cnt_1us + 1'b1;
 end
 
 // 1ms 计数
 always @(posedge s_clk or negedge s_rst) begin
-    if(s_rst == 1'b0)
-        cnt_1ms = 0;
-    else if(cnt==0 && (cnt_1us==0) && (cnt_1ms == K-1))
-        cnt_1ms = 0;
-    else if(cnt==0 && cnt_1us == 0)
+    if(s_rst == 1'b0) begin
+        cnt_1ms = 1'b0;
+        flag = 1'b0;
+    end else if(cnt==1'b0 && (cnt_1us==1'b0) && (cnt_1ms == K-1)) begin
+        cnt_1ms = 1'b0;
+        flag = ~flag;
+    end else if(cnt==1'b0 && cnt_1us == 1'b0)
         cnt_1ms = cnt_1ms + 1'b1;
+    else
+        ;
 end
 
 // led
 always @(posedge s_clk or negedge s_rst) begin
-    if(s_rst == 1'b0)
+    if(s_rst == 1'b0) begin
         led <= 1'b1;
-    else if(cnt_1ms == 0)
-        led <= 1'b1;
-    else if(cnt_1ms >= cnt_1us)
-        led <= 1'b0;
-    else
-        led <= 1'b1;
+    end
+    /*
+    else if(cnt_1ms == 1'b0) begin
+        led <= flag==1'b1 ? 1'b1 : 1'b0;
+    end
+    */
+    else if(cnt_1ms >= cnt_1us) begin
+        led <= flag==1'b1 ? 1'b0 : 1'b1;
+    end else
+        led <= flag==1'b1 ? 1'b1 : 1'b0;
     
 end
 
