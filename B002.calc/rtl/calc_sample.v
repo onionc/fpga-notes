b562235b6d7f1d1624fc411c24ecdc83f92d4ef1
chad <wsrfssx@yeah.net>
2024-03-05 23:54:23 +0800

    fix: 测试组合逻辑赋值等，第一个操作数两次按键正常了。

--- B002.calc/rtl/calc_sample.v
+++ B002.calc/rtl/calc_sample.v
@@ -1,22 +1,17 @@
 // 计算器测试
 
-module calc #(
+module calc_sample #(
     parameter NUM_FOR_200HZ = 60000 // 200Hz计数，12M/200=60000
 )(
 	input clk,
 	input rst_n,
-	
-    // 数码管
-	output rclk_out, // 74HC595 的 RCK 管脚	
-	output sclk_out, // 74HC595 的 SCK 管脚
-	output sdio_out,  // 74HC595 的 SER 管脚
 	
 	// keyboard
     input [3:0] col,
     output  [3:0] row
  
 ); 
-
+/*
 // 数码管配置
 reg	[4:0] 	seg_data_1;		// seg1 数码管要显示的数据
 reg	[4:0]	seg_data_2;		// seg2
@@ -28,14 +23,17 @@ reg	[4:0]	seg_data_7;		// seg7
 reg	[4:0]	seg_data_8;		// seg8
 reg	[7:0]	seg_data_en;	// 数码管数据使能[msb seg8 ~ lsb seg1]
 reg	[7:0]	seg_dot_en;		// 数码管小数点使能
-
+*/
 
 
 
 // 矩阵键盘
 wire [15:0] key_out;
 wire [15:0] key_pulse;
-keyboard keyboard_inst(
+keyboard #(
+	.NUM_FOR_200HZ(NUM_FOR_200HZ)
+)
+keyboard_inst(
 	.clk(clk),
 	.rst_n(rst_n),
 	.col(col),
@@ -44,26 +42,7 @@ keyboard keyboard_inst(
     .key_pulse(key_pulse)
 );
 
-// 数码管
-led_segment inst(
-	.clk(clk),
-    .rst_n(rst_n),
-    .seg_data_1(seg_data_1),
-    .seg_data_2(seg_data_2),
-    .seg_data_3(seg_data_3),
-    .seg_data_4(seg_data_4),
-    .seg_data_5(seg_data_5),
-    .seg_data_6(seg_data_6),
-    .seg_data_7(seg_data_7),
-    .seg_data_8(seg_data_8),
-    .seg_data_en(seg_data_en),
-    .seg_dot_en(seg_dot_en),
-    .rclk_out(rclk_out),
-    .sclk_out(sclk_out),
-    .sdio_out(sdio_out)
-);
-
-
+ 
 // 计算器状态机实现
 
 
@@ -145,9 +124,11 @@ always@(posedge clk or negedge rst_n) begin
 			16'h8000: key_value <= 4'hd;  // /
 			default:  key_value <= key_value;   //无按键按下时保持
 		endcase
-		
-		key_pulse2 <= key_pulse;
-		key_pulse3 <= key_pulse2;
+
+		// 修改1
+
+			key_pulse2 <= key_pulse;
+			key_pulse3 <= key_pulse2;
 
 	/*
 		case(key_pulse)
@@ -179,22 +160,21 @@ end
 // 状态机第二段：使用按键值判断下一个状态（状态转移）
 
 always @(*) begin
-//next_st <=  curr_st;
 	case(curr_st)
 		S00: begin // 只可以输入数字，第一个数字的第一位
-			if((key_value < 4'ha) && (key_pulse2>0) ) begin
+			if((key_value < 4'ha) && key_pulse3>0 ) begin  // 修改2 pulse>0
 				
 				//key_value2 = 4'h2;
-				v11 <= key_value;
+				//v11 <= key_value; 
 				//v111 = 5'd2;
 				next_st = S11;
 			end else 
 				next_st = S00;
 		end
 		S11: begin  // 可以输入数字第二位或者运算符
-			if((key_value < 4'ha) && (key_pulse2>0) ) begin
+			if((key_value < 4'ha) && key_pulse3>0 ) begin
 				
-				v12 = key_value;
+				//v12 = key_value;
 				next_st = S12;
 			end else if((key_value >= 4'ha) && (key_value <= 4'hd)) begin
 				next_st = S_OP;
@@ -249,74 +229,28 @@ always @(*) begin
 			next_st = S00;
 	endcase
 end
-
+reg [4:0] seg_data_1;
+reg [4:0] seg_data_2;
 // 状态机第三段：描述状态输出
 always @(posedge clk or negedge rst_n) begin
 	if(!rst_n) begin
 
 
 		seg_data_1 <= 5'd16; // 16在数码管中置空
-		seg_data_2 <= 5'd16;
-		seg_data_3 <= 5'd16;
-		seg_data_4 <= 5'd16;
-		seg_data_5 <= 5'd16;
-		seg_data_6 <= 5'd16;
-		seg_data_7 <= 5'd16;
-		seg_data_8 <= 5'd16;
 		
-		seg_data_en <= 8'hff; // 所有数码管有效
-		seg_dot_en <= 8'h00;
 	end else begin
-	
-		if(next_st == S11) begin
-			
-			//v111 <= v11;
-			seg_data_1 <= key_value;
-			//seg_data_1 <= 5'd3;
-			// 清除其他数据
-			
-			seg_data_2 <= 5'd16;
-			seg_data_3 <= 5'd16;
-			seg_data_4 <= 5'd16;
-			seg_data_5 <= 5'd16;
-			seg_data_6 <= 5'd16;
-			seg_data_7 <= 5'd16;
-			seg_data_8 <= 5'd16;
 
-		end else if(next_st == S12) begin
-			seg_data_2 <= key_value;
-		end else if(curr_st == S_OP) begin
-			case(op)
-				4'ha: begin // +
-					seg_data_4 <= 5'd10;
-					seg_data_5 <= 5'd11;
-				end
-				4'hb: begin // -
-					seg_data_4 <= 5'd12;
-				end
-				4'hc: begin // *
-					seg_data_4 <= 5'd13;
-				end
-				4'hd: begin
-					seg_data_4 <= 5'd14;
-				end
-				default: begin
-					seg_data_4 <= 5'd15;
-					seg_data_5 <= 5'd15;
-				end
-			endcase
-		end else if(curr_st == S21) begin
-			seg_data_6 <= v21;
-		end else if(curr_st == S22) begin
-			seg_data_7 <= v22;
-		end else if(curr_st == S_EQ) begin
-			seg_data_8 <= 5'd18;
-		end else if(curr_st == S_ERR) begin
-			// ERR
-			seg_data_6 <= 5'd15;
-			seg_data_7 <= 5'd17;
-			seg_data_8 <= 5'd17;
-		end
+			if(next_st == S11) begin // 修改3 next_st
+				
+				//v111 <= v11;
+				seg_data_1 <= key_value;
+				//seg_data_1 <= 5'd3;
+				// 清除其他数据
+				
+
+			end else if(next_st == S12)
+				seg_data_2 <= key_value;
+
 			
 	end
 
