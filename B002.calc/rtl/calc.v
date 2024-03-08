0951361abe3eee725640f7213eb3da4e1bfd4ebd
Chad <wsrfssx@yeah.net>
2024-03-08 13:28:21 +0800

    fix: 组合拆分验证完成。但是输入需要修改，只输入一位，我默认是十位了

--- B002.calc/rtl/calc.v
+++ B002.calc/rtl/calc.v
@@ -3,15 +3,15 @@
 module calc #(
     parameter NUM_FOR_200HZ = 60000 // 200Hz计数，12M/200=60000
 )(
-	input clk,
-	input rst_n,
-	
+    input clk,
+    input rst_n,
+    
     // 数码管
-	output rclk_out, // 74HC595 的 RCK 管脚	
-	output sclk_out, // 74HC595 的 SCK 管脚
-	output sdio_out,  // 74HC595 的 SER 管脚
-	
-	// keyboard
+    output rclk_out, // 74HC595 的 RCK 管脚	
+    output sclk_out, // 74HC595 的 SCK 管脚
+    output sdio_out,  // 74HC595 的 SER 管脚
+    
+    // keyboard
     input [3:0] col,
     output  [3:0] row
  
@@ -36,17 +36,17 @@ reg	[7:0]	seg_dot_en;		// 数码管小数点使能
 wire [15:0] key_out;
 wire [15:0] key_pulse;
 keyboard keyboard_inst(
-	.clk(clk),
-	.rst_n(rst_n),
-	.col(col),
+    .clk(clk),
+    .rst_n(rst_n),
+    .col(col),
     .row(row),
-	.key_out(key_out),
+    .key_out(key_out),
     .key_pulse(key_pulse)
 );
 
 // 数码管
 led_segment inst(
-	.clk(clk),
+    .clk(clk),
     .rst_n(rst_n),
     .seg_data_1(seg_data_1),
     .seg_data_2(seg_data_2),
@@ -89,27 +89,32 @@ reg [3:0] key_value2; // 按键值
 reg [3:0] key_value3; // 按键值
 
 
-reg [3:0] v11; // 操作数1的1位
-reg [3:0] v12; // 操作数1的2位
-reg [3:0] v21; // 操作数2的1位
-reg [3:0] v22; // 操作数2的2位
+reg [3:0] v11; // 操作数1的个位
+reg [3:0] v12; // 操作数1的十位
+reg [3:0] v21; // 操作数2的个位
+reg [3:0] v22; // 操作数2的十位
 reg [3:0] op; // 运算符
 
-reg [7:0] v1; // 操作数1
-reg [7:0] v2; // 操作数2
+wire [7:0] v1; // 操作数1
+wire [7:0] v2; // 操作数2
 
-reg [31:0] v_res; // 结果
+
+reg [7:0] v_res; // 结果，还是只支持8位的结果
+// 计算后将结果再拆分
+wire [3:0] v_r1;
+wire [3:0] v_r2;
+
 
 
 
 // 状态机第一段：同步时序描述状态转移，以及更新按键值
 always @(posedge clk or negedge rst_n) begin
-	if(!rst_n) begin
-		curr_st <= S00;
-	end
-	else begin // 通过按键来判断下一个状态
-		curr_st <= next_st;
-	end
+    if(!rst_n) begin
+        curr_st <= S00;
+    end
+    else begin // 通过按键来判断下一个状态
+        curr_st <= next_st;
+    end
 end
 
 reg flag;
@@ -117,257 +122,270 @@ reg [15:0] key_pulse2;
 reg [15:0] key_pulse3;
 // 更新按键值
 always@(posedge clk or negedge rst_n) begin
-	if(!rst_n) begin
-		key_value <= 4'h0;
-		key_pulse2 <= 16'h0000;
-	end else begin
-	/* 矩阵键盘按键分配如下
-		1 2 3 +
-		4 5 6 -
-		7 8 9 *
-		0 C = /
-		把按键映射到这些数据上，0~9 对应 h'0~9，其他的分别对应 a~f (+ - * / 对应a b c d), f代表清除（也可直接绑定复位），e 代表计算=
-	*/
-	
-		case(key_pulse)
-			16'h0001: key_value <= 4'h1;  // 1
-			16'h0002: key_value <= 4'h2;  // 2
-			16'h0004: key_value <= 4'h3;  // 3
-			16'h0008: key_value <= 4'ha;  // +
-			16'h0010: key_value <= 4'h4;  // 4
-			16'h0020: key_value <= 4'h5;  // 5
-			16'h0040: key_value <= 4'h6;  // 6
-			16'h0080: key_value <= 4'hb;  // -
-			16'h0100: key_value <= 4'h7;  // 7
+    if(!rst_n) begin
+        key_value <= 4'h0;
+        key_pulse2 <= 16'h0000;
+    end else begin
+    /* 矩阵键盘按键分配如下
+        1 2 3 +
+        4 5 6 -
+        7 8 9 *
+        0 C = /
+        把按键映射到这些数据上，0~9 对应 h'0~9，其他的分别对应 a~f (+ - * / 对应a b c d), f代表清除（也可直接绑定复位），e 代表计算=
+    */
+    
+        case(key_pulse)
+            16'h0001: key_value <= 4'h1;  // 1
+            16'h0002: key_value <= 4'h2;  // 2
+            16'h0004: key_value <= 4'h3;  // 3
+            16'h0008: key_value <= 4'ha;  // +
+            16'h0010: key_value <= 4'h4;  // 4
+            16'h0020: key_value <= 4'h5;  // 5
+            16'h0040: key_value <= 4'h6;  // 6
+            16'h0080: key_value <= 4'hb;  // -
+            16'h0100: key_value <= 4'h7;  // 7
             16'h0200: key_value <= 4'h8;  // 8
-			16'h0400: key_value <= 4'h9;  // 9
-			16'h0800: key_value <= 4'hc;  // x
-			16'h1000: key_value <= 4'h0;  // 0
-			16'h2000: key_value <= 4'hf;  // C 清除（重置） ，f也做无效值用
-			16'h4000: key_value <= 4'he;  // =
-			16'h8000: key_value <= 4'hd;  // /
-			default:  key_value <= key_value;   //无按键按下时保持
-		endcase
-		
-		key_pulse2 <= key_pulse; // 延迟一拍，在组合逻辑中使用
-	end
+            16'h0400: key_value <= 4'h9;  // 9
+            16'h0800: key_value <= 4'hc;  // x
+            16'h1000: key_value <= 4'h0;  // 0
+            16'h2000: key_value <= 4'hf;  // C 清除（重置） ，f也做无效值用
+            16'h4000: key_value <= 4'he;  // =
+            16'h8000: key_value <= 4'hd;  // /
+            default:  key_value <= key_value;   //无按键按下时保持
+        endcase
+        
+        key_pulse2 <= key_pulse; // 延迟一拍，在组合逻辑中使用
+    end
 end
 
 // 状态机第二段：使用按键值判断下一个状态（状态转移）
 
 always @(*) begin
-	next_st =  curr_st; // 这里或者在所有else中赋值
+    next_st =  curr_st; // 这里或者在所有else中赋值
 
-	if((key_value == 4'hf) && (key_pulse2>0) ) begin // 复位
-		next_st = S00;
-	end else
-	case(next_st)
-		S00: begin // 只可以输入数字，第一个数字的第一位
-			if((key_value < 4'ha) && (key_pulse2>0) ) begin
-				
-				next_st = S11;
-			end
-		end
-		S11: begin  // 可以输入数字第二位或者运算符
-			if((key_value < 4'ha) && (key_pulse2>0) ) begin
-				next_st = S12;
-			end else if((key_value >= 4'ha) && (key_value <= 4'hd) && (key_pulse2>0)  ) begin
-				next_st = S_OP;
-			end 
+    if((key_value == 4'hf) && (key_pulse2>0) ) begin // 复位
+        next_st = S00;
+    end else
+    case(next_st)
+        S00: begin // 只可以输入数字，第一个数字的第一位
+            if((key_value < 4'ha) && (key_pulse2>0) ) begin
+                
+                next_st = S11;
+            end
+        end
+        S11: begin  // 可以输入数字第二位或者运算符
+            if((key_value < 4'ha) && (key_pulse2>0) ) begin
+                next_st = S12;
+            end else if((key_value >= 4'ha) && (key_value <= 4'hd) && (key_pulse2>0)  ) begin
+                next_st = S_OP;
+            end 
 
-		end
+        end
 
-		S12: begin // 只可以输入运算符
-			if((key_value >= 4'ha) && (key_value <= 4'hd) && (key_pulse2>0)) begin
-				next_st = S_OP;
-			end 
-		end
+        S12: begin // 只可以输入运算符
+            if((key_value >= 4'ha) && (key_value <= 4'hd) && (key_pulse2>0)) begin
+                next_st = S_OP;
+            end 
+        end
 
-		S_OP: begin // 可以输入第二数字的第一位
-		  	if((key_value < 4'ha) && (key_pulse2>0)) begin
-				next_st = S21;
-			end 
-		end
+        S_OP: begin // 可以输入第二数字的第一位
+              if((key_value < 4'ha) && (key_pulse2>0)) begin
+                next_st = S21;
+            end 
+        end
 
-		S21: begin  // 可以输入数字第二位或者等号
-			if((key_value < 4'ha) && (key_pulse2>0)) begin
-				next_st = S22;
-			end else if((key_value == 4'he) && (key_pulse2>0)) begin
-				next_st = S_EQ;
-			end 
-		end
+        S21: begin  // 可以输入数字第二位或者等号
+            if((key_value < 4'ha) && (key_pulse2>0)) begin
+                next_st = S22;
+            end else if((key_value == 4'he) && (key_pulse2>0)) begin
+                next_st = S_EQ;
+            end 
+        end
 
-		S22: begin  // 只可以输入等号
-			if((key_value == 4'he) && (key_pulse2>0)) begin
-				next_st = S_EQ;
-			end 
-		end
+        S22: begin  // 只可以输入等号
+            if((key_value == 4'he) && (key_pulse2>0)) begin
+                next_st = S_EQ;
+            end 
+        end
 
-		S_EQ: begin // 等待计算
-			//next_st = S00;
-		end
+        S_EQ: begin // 等待计算
+            //next_st = S00;
+        end
 
-		S_ERR: begin
-			next_st = S_ERR;
-		end
+        S_ERR: begin
+            next_st = S_ERR;
+        end
 
-		default:
-			next_st = S00;
-	endcase
+        default:
+            next_st = S00;
+    endcase
 end
 
 
 
 // 状态机第三段：描述状态输出
 always @(posedge clk or negedge rst_n) begin
-	if(!rst_n) begin
+    if(!rst_n) begin
 
 
-		seg_data_1 <= 5'd16; // 16在数码管中置空
-		seg_data_2 <= 5'd16;
-		seg_data_3 <= 5'd16;
-		seg_data_4 <= 5'd16;
-		seg_data_5 <= 5'd16;
-		seg_data_6 <= 5'd16;
-		seg_data_7 <= 5'd16;
-		seg_data_8 <= 5'd16;
-		
-		seg_data_en <= 8'hff; // 所有数码管有效
-		seg_dot_en <= 8'h00;
-	end else begin
-		if(next_st == S00 ) begin // 复位
-			seg_data_1 <= 5'd16; // 16在数码管中置空
-			seg_data_2 <= 5'd16;
-			seg_data_3 <= 5'd16;
-			seg_data_4 <= 5'd16;
-			seg_data_5 <= 5'd16;
-			seg_data_6 <= 5'd16;
-			seg_data_7 <= 5'd16;
-			seg_data_8 <= 5'd16;
-		
-			seg_data_en <= 8'hff; // 所有数码管有效
-			seg_dot_en <= 8'h00;
-		end else if(next_st == S11) begin
-			
-			if(key_value < 4'ha) begin // 为避免在S11时按其他键导致问题，这里还需判断一次
-				
-				v11 <= key_value;
-				v12 <= 4'h0; // v12 第二位可能不会被赋值，这里来赋值
-				seg_data_1 <= key_value;
-				
+        seg_data_1 <= 5'd16; // 16在数码管中置空
+        seg_data_2 <= 5'd16;
+        seg_data_3 <= 5'd16;
+        seg_data_4 <= 5'd16;
+        seg_data_5 <= 5'd16;
+        seg_data_6 <= 5'd16;
+        seg_data_7 <= 5'd16;
+        seg_data_8 <= 5'd16;
+        
+        seg_data_en <= 8'hff; // 所有数码管有效
+        seg_dot_en <= 8'h00;
+    end else begin
+        if(next_st == S00 ) begin // 复位
+            seg_data_1 <= 5'd16; // 16在数码管中置空
+            seg_data_2 <= 5'd16;
+            seg_data_3 <= 5'd16;
+            seg_data_4 <= 5'd16;
+            seg_data_5 <= 5'd16;
+            seg_data_6 <= 5'd16;
+            seg_data_7 <= 5'd16;
+            seg_data_8 <= 5'd16;
+        
+            seg_data_en <= 8'hff; // 所有数码管有效
+            seg_dot_en <= 8'h00;
+        end else if(next_st == S11) begin
+            
+            if(key_value < 4'ha) begin // 为避免在S11时按其他键导致问题，这里还需判断一次
+                
+                v11 <= key_value;
+                v12 <= 4'h0; // v12 第二位可能不会被赋值，这里来赋值
+                seg_data_1 <= key_value;
+                
 
-				// 清除其他数据
-				seg_data_2 <= 5'd16;
+                // 清除其他数据
+                seg_data_2 <= 5'd16;
+                seg_data_3 <= 5'd16;
+                seg_data_4 <= 5'd16;
+                seg_data_5 <= 5'd16;
+                seg_data_6 <= 5'd16;
+                seg_data_7 <= 5'd16;
+                seg_data_8 <= 5'd16;
+            end
+            
+
+        end else if(next_st == S12) begin
+            if(key_value < 4'ha) begin
+                v12 <= key_value;
+
+                seg_data_2 <= key_value;
+            end
+        end else if(next_st == S_OP) begin
+            if((key_value >= 4'ha) && (key_value <= 4'hd)) begin
+                op <= key_value;
+
+                // 最多会用3个数码管显示符号，不用时也置为空，不然可能会是上一次的值
+                case(op)
+                    4'ha: begin // +
+                        seg_data_3 <= 5'd16;
+                        seg_data_4 <= 5'd10;
+                        //seg_data_5 <= 5'd11;
+                        seg_data_5 <= 5'd12;
+                    end
+                    4'hb: begin // -
+                        seg_data_3 <= 5'd16;
+                        seg_data_4 <= 5'd12;
+                        seg_data_5 <= 5'd16;
+                    end
+                    4'hc: begin // *
+                        seg_data_3 <= 5'd16;
+                        seg_data_4 <= 5'd13;
+                        seg_data_5 <= 5'd16;
+                    end
+                    4'hd: begin // /
+                        seg_data_3 <= 5'd12;
+                        seg_data_4 <= 5'd14;
+                        seg_data_5 <= 5'd12;
+                    end
+                    default: begin
+                        seg_data_3 <= 5'd16;
+                        seg_data_4 <= 5'd15;
+                        seg_data_5 <= 5'd15;
+                    end
+                endcase
+            end
+        end else if(next_st == S21) begin
+            if(key_value < 4'ha) begin
+                seg_data_6 <= key_value;
+                v21 <= key_value;
+                v22 <= 4'h0;
+            end
+        end else if(next_st == S22) begin
+            if(key_value < 4'ha) begin
+                seg_data_7 <= key_value;
+                v22 <= key_value;
+            end
+        end else if(next_st == S_EQ) begin
+            if(key_value == 4'he) begin
+				// 等号以及其他不用的位
+				seg_data_1 <= 5'd18;
+                seg_data_2 <= 5'd16;
 				seg_data_3 <= 5'd16;
 				seg_data_4 <= 5'd16;
 				seg_data_5 <= 5'd16;
-				seg_data_6 <= 5'd16;
-				seg_data_7 <= 5'd16;
-				seg_data_8 <= 5'd16;
-			end
-			
+                seg_data_6 <= 5'd16;
+                seg_data_7 <= 5'd16;
+                seg_data_8 <= 5'd16;
 
-		end else if(next_st == S12) begin
-			if(key_value < 4'ha) begin
-				v12 <= key_value;
+                // 计算
+                case(op)
+                    4'ha: begin
+                        v_res <= v1 + v2;
+						if(v_res>8'd100) v_res <= v_res-8'd100;
+                        seg_data_3 <= {0'b0, v_r1};
+                		seg_data_4 <= {0'b0, v_r2};
+                    end
+                    /*
+                    4'hb: begin
 
-				seg_data_2 <= key_value;
-			end
-		end else if(next_st == S_OP) begin
-			if((key_value >= 4'ha) && (key_value <= 4'hd)) begin
-				op <= key_value;
+                    end
+                    4'hc: begin
+                    end
+                    4'hd: begin
+                    end
+                    */
+                    default:
+                        v_res <= 32'd0;
+                endcase
 
-				// 最多会用3个数码管显示符号，不用时也置为空，不然可能会是上一次的值
-				case(op)
-					4'ha: begin // +
-						seg_data_3 <= 5'd16;
-						seg_data_4 <= 5'd10;
-						//seg_data_5 <= 5'd11;
-						seg_data_5 <= 5'd12;
-					end
-					4'hb: begin // -
-						seg_data_3 <= 5'd16;
-						seg_data_4 <= 5'd12;
-						seg_data_5 <= 5'd16;
-					end
-					4'hc: begin // *
-						seg_data_3 <= 5'd16;
-						seg_data_4 <= 5'd13;
-						seg_data_5 <= 5'd16;
-					end
-					4'hd: begin // /
-						seg_data_3 <= 5'd12;
-						seg_data_4 <= 5'd14;
-						seg_data_5 <= 5'd12;
-					end
-					default: begin
-						seg_data_3 <= 5'd16;
-						seg_data_4 <= 5'd15;
-						seg_data_5 <= 5'd15;
-					end
-				endcase
-			end
-		end else if(next_st == S21) begin
-			if(key_value < 4'ha) begin
-				seg_data_6 <= key_value;
-				v21 <= key_value;
-				v22 <= 4'h0;
-			end
-		end else if(next_st == S22) begin
-			if(key_value < 4'ha) begin
-				seg_data_7 <= key_value;
-				v22 <= key_value;
-			end
-		end else if(next_st == S_EQ) begin
-			if(key_value == 4'he) begin
-				// 计算
-				//seg_data_8 <= 5'd18;
 
-				// 操作数1，默认都正数 v11*10+v12
-				v1 <= {v11, 3'b0} + {v11, 1'b0} + v12;
-				
-				// 操作数2
-				v2 <= {v21, 3'b0} + {v12, 1'b0} + v12;
-				
-				
-				case(op)
-					4'ha: begin
-						v_res <= (v11*10+v12) + (v21*10 + v22);
-					end
-					/*
-					4'hb: begin
-
-					end
-					4'hc: begin
-					end
-					4'hd: begin
-					end
-					*/
-					default:
-						v_res <= 32'd0;
-				endcase
-
-				seg_data_1 <= 5'd18;
-				seg_data_2 <= 5'd16;
-				seg_data_3 <= 5'd16;
-				seg_data_6 <= 5'd16;
-				seg_data_7 <= 5'd16;
-				seg_data_8 <= 5'd16;
-
-				seg_data_3 <= (v_res/100)%10;
-				seg_data_4 <= (v_res/10)%10;
-				seg_data_5 <= v_res%10;
-
-			end
-		end else if(next_st == S_ERR) begin
-			// ERR
-			seg_data_6 <= 5'd15;
-			seg_data_7 <= 5'd17;
-			seg_data_8 <= 5'd17;
-		end
-			
-	end
+            end
+        end else if(next_st == S_ERR) begin
+            // ERR
+            seg_data_6 <= 5'd15;
+            seg_data_7 <= 5'd17;
+            seg_data_8 <= 5'd17;
+        end
+            
+    end
 
 end
+
+// 拼接
+num_join num_join_inst1(
+    .v1(v11),
+    .v2(v12),
+    .vout(v1) 
+);
+num_join num_join_inst2(
+    .v1(v21),
+    .v2(v22),
+    .vout(v2) 
+);
+
+// 再拆分
+num_split num_split_inst1(
+    .v(v_res),
+    .v1(v_r1),
+    .v2(v_r2)
+);
 
 endmodule
