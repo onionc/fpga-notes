6ab8e29d12ee2215eab2ec247a0af64bb5e226af
chad <wsrfssx@yeah.net>
2024-03-09 10:11:47 +0800

    fix: 负数除法，负号单拎出来（解决-0补码没法表示的问题）

--- B002.calc/rtl/calc_div.v
+++ B002.calc/rtl/calc_div.v
@@ -4,7 +4,8 @@ module calc_div(
     input [7:0] A, // 被除数
     input [7:0] B, // 除数
     output reg [7:0]  Q, // 商
-    output reg [7:0]  R  // 余数
+    output reg [7:0]  R, // 余数
+    output reg neg // 负号
 );
 
 // 除法
@@ -24,14 +25,16 @@ always @(*) begin
     */
     R = 8'b0;
     Q = 8'b0;
+    neg = 1'b0;
    
     // 负数转正数
-    if(A[7] == 1'b1)
-        A2 = (~A + 1'b1);
-    else
+    if(A[7] == 1'b1) begin
+        A2 = {1'b0, ~A[6:0]} + 1'b1;
+
+    end else
         A2 = A;
     if(B[7] == 1'b1)
-        B2 = (~B + 1'b1);
+        A2 = {1'b0, ~B[6:0]} + 1'b1;
     else
         B2 = B;
 
@@ -47,8 +50,10 @@ always @(*) begin
     end
 
     // 负数再转回去
-    if(A[7]^B[7])
-        Q = {1'b1, ~Q[6:0] + 1'b1};
+    if(A[7]^B[7]) begin
+        // Q = {1'b1, ~Q[6:0]} + 1'b1; //不在商里面加负号，因为-0没法表示，0和-0补码一样
+        neg = 1'b1;
+    end
 end
 
 endmodule
