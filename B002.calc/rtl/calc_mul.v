1dbd306b3016ed0815ab1c64104c3fcb3ed4d762
chad <wsrfssx@yeah.net>
2024-03-09 23:30:51 +0800

    feat: 两位10进制四则运算计算器完成，虽然还有点瑕疵（对负数支持不到位等），但基本可用。除法也能显示一位小数了。ye~~~~

--- B002.calc/rtl/calc_mul.v
+++ B002.calc/rtl/calc_mul.v
@@ -1,4 +1,4 @@
-// (无符号）8位乘法
+// 8位乘法
 
 module calc_mul(
     input [7:0] A, // 被乘数
@@ -7,15 +7,28 @@ module calc_mul(
 );
 
 integer i;
+reg [15:0] A2; // 扩展
+reg [15:0] B2;
 always @(*) begin
 
     out = 16'b0;
-   
-    for(i=0; i<=7; i=i+1) begin
-        if(B[i]==1) begin
-            out = out + A<<i;
+
+    // 负数扩展
+    if(A[7]==1'b1)
+        A2 = {8'hff, A};
+    else
+        A2 = A;
+    if(B[7]==1'b1)
+        B2 = {8'hff, B};
+    else
+        B2 = B;
+
+    for(i=0; i<=15; i=i+1) begin
+        if(B2[i]==1) begin
+            out = out + (A2<<i);
         end
     end
+
 end
 
 endmodule
